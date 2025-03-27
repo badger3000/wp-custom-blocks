1a63a623d97f19cb6c267f26a9071c96d2e22b97
Kyle Ross <kyle@badger3000.com>
2025-03-27 08:50:46 -0700

    track build files
    - tracking the build files

---
+++ build/blocks/calculator/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b98e89428ae696797a1c');
---
+++ build/blocks/sample-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b05f9b96be60e45c5d34');
